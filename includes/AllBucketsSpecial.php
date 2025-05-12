39e7cfc817280f3317797f1aae121b131fc581a0
Morgan Lewis <MESLewis@gmail.com>
2025-05-12 15:54:52 -0500

    Rudimentary Special:AllBuckets page

---
+++ includes/AllBucketsSpecial.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace MediaWiki\Extension\Bucket;
+
+use MediaWiki\SpecialPage\SpecialPage;
+use MediaWiki\MediaWikiServices;
+
+class AllBucketsSpecial extends SpecialPage {
+    public function __construct() {
+        parent::__construct( 'allbuckets' );
+    }
+
+    public function execute( $par ) {
+        $request = $this->getRequest();
+		$out = $this->getOutput();
+		$this->setHeaders();
+
+        $out->setPageTitle( "All Buckets" );
+
+        $dbw = MediaWikiServices::getInstance()->getDBLoadBalancer()->getConnectionRef( DB_PRIMARY );
+        $res = $dbw->newSelectQueryBuilder()
+            ->from('bucket_schemas')
+            ->select(['table_name', 'backing_table_name', 'schema_json'])
+            ->caller(__METHOD__)
+            ->fetchResultSet();
+
+		$schemas = [];
+		$backingBucketName = [];
+		foreach ( $res as $row ) {
+			$schemas[$row->table_name] = json_decode( $row->schema_json, true );
+			$backingBucketName[$row->table_name] = $row->backing_table_name;
+		}
+
+        $output = [];
+
+        $output[] = "<table class=\"wikitable\"><tr>";
+        $output[] = "<th>Bucket</th><th>Backing Bucket</th>";
+        foreach( $schemas as $row => $val ) {
+            $output[] = "<tr>";
+            $output[] = "<td>[[Bucket:$row]]</td>";
+            if ( $backingBucketName[$row] != null ) {
+                $output[] = "<td>[[Bucket:$backingBucketName[$row]]]</td>";
+            } else {
+                $output[] = "<td></td>";
+            }
+            $output[] = "</tr>";
+        }
+        $output[] = "</table>";
+
+        $out->addWikiTextAsContent(implode('', $output));
+
+    }
+
+    function getGroupName() {
+        return 'bucket';
+    }
+}
