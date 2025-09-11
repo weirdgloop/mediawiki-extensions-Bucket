04d1632cc746dc9db9bf6447fb736a737022700e
Bilin Tsui <xuminruixuan@live.cn>
2025-09-11 12:03:24 +0800

    i18n: Add ability to localize namespaces

---
+++ Bucket.i18n.namespaces.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Internationalisation file for extension Bucket.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$namespaceNames = [];
+
+$namespaceAliases = [];
+
+$namespaceNames['en'] = [
+	9592 => 'Bucket',
+	9593 => 'Bucket_talk',
+];
