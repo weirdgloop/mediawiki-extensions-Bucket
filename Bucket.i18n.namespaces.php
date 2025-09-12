24dea32fb93f7df556bc3c13c8b77b4285db3178
Yurii <111344205+METROKOP228@users.noreply.github.com>
2025-09-13 00:44:28 +0300

    Fixed a mistake

--- Bucket.i18n.namespaces.php
+++ Bucket.i18n.namespaces.php
@@ -15,14 +15,14 @@ $namespaceNames['en'] = [
 	9593 => 'Bucket_talk',
 ];
 
-$namespaceNames['en'] = [
-	9592 => 'Bucket',
-	9593 => 'Bucket_talk',
-];
-
 $namespaceNames['uk'] = [
 	9592 => 'Bucket',
 	9593 => 'Обговорення_Bucket',
+];
+
+$namespaceNames['zh'] = [
+	9592 => 'Bucket',
+	9593 => 'Bucket_talk',
 ];
 
 $namespaceNames['zh-hans'] = [
